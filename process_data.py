4884418a771f62b1376b74929f2e6ee50bb3b13a
Sami Khan <samikhan10902@gmail.com>
2026-05-20 19:12:52 +0530

    Completed Task 2 data processing

---
+++ process_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df1 = pd.read_csv("data/daily_sales_data_0.csv")
+df2 = pd.read_csv("data/daily_sales_data_1.csv")
+df3 = pd.read_csv("data/daily_sales_data_2.csv")
+
+df = pd.concat([df1, df2, df3])
+
+df = df[df["product"] == "Pink Morsel"]
+
+df["price"] = df["price"].replace("[$]", "", regex=True)
+
+df["price"] = df["price"].astype(float)
+
+df["sales"] = df["quantity"] * df["price"]
+
+df = df[["sales", "date", "region"]]
+
+df.to_csv("formatted_sales_data.csv", index=False)
+
+print("Data processing completed!")
